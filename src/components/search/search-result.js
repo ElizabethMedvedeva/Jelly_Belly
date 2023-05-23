b5c3cb351176325a1ad59e07b4f9df688c3a995a
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2023-05-23 14:42:01 +0300

    add event listener to search cards

--- src/components/search/search-result.js
+++ src/components/search/search-result.js
@@ -2,8 +2,12 @@ import "./search-result.less";
 
 import { createElement } from "../../helpers/index.js";
 
+import { chosenCardsKey } from "../../core/constants.js";
+
+import { getStore, setStore } from "../../core/store";
+
 export const SearchResult = (product) => {
-  return createElement({
+  const searchResultElement = createElement({
     name: "li",
     classList: ["main-search__result-product", "result-product"],
     childrens: [
@@ -17,9 +21,15 @@ export const SearchResult = (product) => {
       {
         name: "div",
         classList: ["result-product__info"],
+        attributes: { id: product.id },
         childrens: [
           { name: "h4", text: product.name },
           { name: "h5", text: `${product.price} $` },
+          {
+            name: "div",
+            classList: ["result-product__article"],
+            childrens: [{ name: "h5", text: `article: ${product.id}` }],
+          },
         ],
       },
       {
@@ -34,4 +44,18 @@ export const SearchResult = (product) => {
       },
     ],
   });
+  const basketSearchBtn = searchResultElement.querySelector(".icon-cart");
+  basketSearchBtn.addEventListener("click", addToBasket);
+  return searchResultElement;
 };
+function addToBasket(event) {
+  let cardsID = getStore(chosenCardsKey);
+  if (!cardsID) {
+    cardsID = [];
+  }
+  let fullCard = event.target.parentNode.parentNode.parentNode;
+  let cardResultProduct = fullCard.querySelector(".result-product__info");
+  let cardID = cardResultProduct.id;
+  cardsID.push(cardID);
+  setStore(chosenCardsKey, cardsID);
+}
